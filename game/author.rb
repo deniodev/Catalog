8b9adb434103e5af34548eeb85006a29d7fe4bac
Fezzopro <fezzopro@gmail.com>
2023-08-10 17:13:06 +0200

    Add author class to the program

---
+++ game/author.rb
@@ -0,0 +1,25 @@
+class Author
+  attr_reader :id, :items
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @id = rand(1_000_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    items << item unless items.includes?(item)
+    item.author = self
+  end
+
+  def to_json
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name,
+      item: @item
+    }
+  end
+end
