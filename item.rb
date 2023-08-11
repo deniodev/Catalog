1847850080c1f7f973ab0f51c2f91bc89849665e
Denio Nhanale <dnhanale@gmail.com>
2023-08-11 08:56:53 +0200

    Update Item class

--- item.rb
+++ item.rb
@@ -1,20 +1,48 @@
 class Item
-  attr_reader :id
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :archived, :genre
+  attr_accessor :author, :source, :label, :publish_date
 
-  def initialize(publish_date, archived: false)
-    @id = rand(1_000)
+  def initialize(publish_date)
+    @id = rand(1...1000)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
 
   def can_be_archived?
-    Time.now.year - publish_date.year > 10
+    arc = Time.now.year - @publish_date
+
+    arc > 10
   end
 
   def move_to_archive
-    @archived = can_be_archived?
+    return unless can_be_archived?
+
+    @archived = true
   end
 
   private :can_be_archived?
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
 end
